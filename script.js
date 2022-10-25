5aded7999d6b12141c32233c584c0fd327d0e6cb
Facem <f_maggi@outlook.com>
2022-10-25 10:57:41 -0300

    Agregado fetch de api's

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
-    alert("Jquery listo")
+    fetch("https://www.dolarsi.com/api/api.php?type=valoresprincipales")
+    .then(response=>response.json())
+    .then(data=> console.log(data))
+
+    fetch("https://api.openweathermap.org/data/2.5/weather?lat=-34.60&lon=-58.450&appid=1e50bca7f29b7d6f1db873e141174af1")
+    .then(response=>response.json())
+    .then(data=>console.log(data))
 })
+
+
